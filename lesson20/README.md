82f3b40ead1b04a365b09fb8a49ec4a551c39f16
DaveatCor <condaveat@gmail.com>
2019-08-25 10:34:58 +0700

    add item to database

--- lesson20/README.md
+++ lesson20/README.md
@@ -6,6 +6,7 @@ And we use sqlite with our Apps (Flutter).
 On Flutter we use library name SQFlite.
 - [Check SQFLite Docs](https://pub.dev/packages/sqflite)
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Directory is an instance temporary 
+
+Query data from database
+First argument: Name of table that created 
---
+++ lesson20/lib/src/model/model.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+
+class ItemModel {
+  final int id;
+  final bool deleted;
+  final String type;
+  final String by;
+  final int time;
+  final bool dead;
+  final String parent;
+  final List<dynamic> kids;
+  final String url;
+  final int score;
+  final String title;
+  final int descendants;
+
+  ItemModel.fromJson(Map<String, dynamic> parse) 
+    : id = parse['id'],
+      deleted = parse['deleted'],
+      type = parse['type'],
+      by = parse['by'],
+      time = parse['time'],
+      dead = parse['dead'],
+      parent = parse['parent'],
+      kids = parse['kids'],
+      url = parse['url'],
+      score = parse['score'],
+      title = parse['title'],
+      descendants = parse['descendants'];
+
+  ItemModel.fromDb(Map<String, dynamic> parse) 
+    : id = parse['id'],
+      deleted = parse['deleted'] == 1,
+      type = parse['type'],
+      by = parse['by'],
+      time = parse['time'],
+      dead = parse['dead'] == 1,
+      parent = parse['parent'],
+      kids = jsonDecode(parse['kids']),
+      url = parse['url'],
+      score = parse['score'],
+      title = parse['title'],
+      descendants = parse['descendants'];
+
+  toMap() {
+    return <String, dynamic> {
+      "id" : id,
+      "deleted" : deleted ? 1 : 0,
+      "type" : type,
+      "by" : by,
+      "time" : time,
+      "dead" : dead ? 1 : 0,
+      "parent" : parent,
+      "kids" : jsonEncode(kids),
+      "url" : url,
+      "score" : score,
+      "title" : title,
+      "descendants" : descendants
+    };
+  }
+    
+}
