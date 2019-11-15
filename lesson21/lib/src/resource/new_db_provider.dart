58d4f2a672b0ab09daa233be8b53b356ccff0df8
DaveatCor <condaveat@gmail.com>
2019-11-15 10:24:36 +0700

    make instance variable in NewsDbProivder

---
+++ lesson21/lib/src/resource/new_db_provider.dart
@@ -0,0 +1,69 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'dart:async';
+import 'package:lesson21/src/model/model.dart';
+import 'respository.dart';
+
+class NewsDbProvider implements Source, Cache{
+
+  /* Todo - Store and fetch Top Ids */
+  Future<List<int>> fetchTopIds() {
+    return null;
+  }
+
+  Database db;
+  void init() async {
+    /* Reference to directory or fileSystem on mobile devices*/
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentDirectory.path, "items.db"); 
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database newDb, int version){
+        /* Items below is a name of Database */
+        /* BLOB is a big set of data */
+        /* Sequel lite doesn't have boolean type but we can assign variable as integer and 0 == false, 1 == true */
+        newDb.execute("""
+          CREATE TABLE Items(
+            id INTEGER PRIMARY KEY,
+            deleted INTEGER,
+            type TEXT,
+            by TEXT,
+            time INTEGER,
+            dead INTEGER,
+            parent TEXT,
+            kids BLOB,
+            url TEXT,
+            score INTEGER,
+            title TEXT,
+            descendants INTEGER
+          )
+        """);
+      }
+    );
+  }
+
+  Future<ItemModel> fetchItem(int id) async {
+    final maps = await db.query(
+      "Items",
+      columns: null,
+      /* Where is a query class that specify actual search */
+      where: "id = ?", /* The question mark is get parameter from id and will replace whereArgs list */
+      whereArgs: [id] /* WhereArges is declare for prevent SQL Injection (SQL Injection Attack) */
+    );
+
+    if (maps.length > 0) {
+      return ItemModel.fromDb(maps.first);
+    }
+
+    return null;
+  }
+
+  addItem(ItemModel model) {
+    db.insert("Items", model.toMap());
+  }
+}
+
+final newsDbProvider = NewsDbProvider();
---
+++ lesson21/lib/src/resource/respository.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+import 'new_db_provider.dart';
+import 'new_api_provider.dart';
+import 'package:lesson21/src/model/model.dart';
+
+class Respository {
+  List<Source> sources = <Source> [
+    newsDbProvider,
+    NewsApiProvider(),
+  ];
+
+  List<Cache> caches = <Cache> [
+    newsDbProvider
+  ];
+
+  fetchTopIds() {
+    return null;
+  }
+  
+  Future<ItemModel> fetchItem(int id) async {
+    ItemModel item;
+    Source source;
+    int i = 1;
+
+    for (source in sources ){
+      item = await source.fetchItem(id);
+      
+      /* If Have Data Break Loop */
+      if ( item != null ) {
+        print(item);
+        print(i);
+        break;
+      }
+      i++;
+    }
+
+    for (var cache in caches){
+      cache.addItem(item);
+    }
+
+    return item;
+    // var item = await newsDbProvider.fetchItem(id); 
+    // if ( item != null ) {
+    //   return item;
+    // }
+
+    // item = await newsApiProvider.fetchItem(id);
+    // newsDbProvider.addItem(item);
+    // return item;
+  }
+
+}
+abstract class Source {
+  Future<List<int>> fetchTopIds();
+  Future<ItemModel> fetchItem(int id);
+}
+
+abstract class Cache {
+ Future<ItemModel> addItem(ItemModel item);
+}
