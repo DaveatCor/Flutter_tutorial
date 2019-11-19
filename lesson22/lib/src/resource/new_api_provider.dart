23a4e7e8a38803a54d37a1b621f2926636a24f43
DaveatCor <condaveat@gmail.com>
2019-11-19 10:33:07 +0700

    4. Solution Outline

--- lesson22/lib/src/resource/new_api_provider.dart
+++ lesson22/lib/src/resource/new_api_provider.dart
@@ -13,10 +13,10 @@ class NewsApiProvider implements Source{
     /* Decode Json Data to default Type */
     final id = await json.decode(response.body);
     /* Default Decode Type Is List Dynamic, And Below Convert That Type To List Int */
-    List<int> toListInt = List<int>.from(id);
+    // List<int> toListInt = List<int>.from(id);
     /* Convert List Int To Future List Int To Fit With Function Type*/
-    Future<List<int>> listInt = new Future<List<int>>(() => toListInt );
-    return listInt;
+    // Future<List<int>> listInt = new Future<List<int>>(() => toListInt );
+    return id;
   }
   Future<ItemModel> fetchItem(int id) async {
     final response = await client.get('$_root/item/$id.json');
--- lesson22/lib/src/screens/news_list.dart
+++ lesson22/lib/src/screens/news_list.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class NewsList extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('News List'),
+        title: Text('Top news'),
       ),
-      body: Text('Show some news list'),
+      body: getList(),
+    );
+  }
+
+  Widget getList() {
+    return ListView.builder(
+      itemCount: 1000,
+      itemBuilder: (context, index) {
+        return FutureBuilder(
+          future: getFuture(),
+          builder: (context, snapshot) {
+            return Container(
+              height: 80.0,
+              child: snapshot.hasData 
+                ? Text("Im visible $index") 
+                : Text("Im havent fetch data yey $index"),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  getFuture() {
+    return Future.delayed(
+      Duration(seconds: 2),
+      () => 'Hi'
     );
   }
 }
